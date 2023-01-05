d70d3da34cafd8c08ef27f529ab63d24366a4d98
Jed Purdy <Jed.Purdy@etu.univ-smb.fr>
2023-01-05 09:09:22 +0100

    jsp tjr

---
+++ MarsInvader/MarsInvader/alien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsInvader
+{
+    internal class alien
+    {
+        private int health;
+        private int attack;
+        private int points;
+        private int speed;
+        private int niveau;
+    }
+}
